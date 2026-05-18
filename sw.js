f3b3e91851d2f492a647ca330e65560079d3d28b
uwanaemmanuel500-dev <uwanaemmanuel500@gmail.com>
2026-05-18 10:06:17 +0100

    Simplify install and fetch event listeners in sw.js
    Refactor service worker event listeners for clarity.

--- sw.js
+++ sw.js
@@ -1,31 +1,10 @@
 const CACHE_NAME = 'topx-v1';
-const FILES_TO_CACHE = [
-  './game.html',
-  './'
-];
+const FILES_TO_CACHE = ['./game.html', './'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
