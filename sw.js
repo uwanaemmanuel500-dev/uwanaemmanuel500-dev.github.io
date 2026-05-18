307cd3611273bb698f3d73db1ba4a3eea7600850
uwanaemmanuel500-dev <uwanaemmanuel500@gmail.com>
2026-05-18 10:29:20 +0100

    Update service worker to cache additional files

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
 const CACHE_NAME = 'topx-v1';
-const FILES_TO_CACHE = ['./game.html', './'];
+const FILES_TO_CACHE = ['./', './index.html', './style.css'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
